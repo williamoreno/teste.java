617a0327d789e251437672815e67464eeb5295c1
williamoreno <53883625+williamoreno@users.noreply.github.com>
2019-08-29 21:31:22 -0300

    4° questão do teste.

---
+++ teste.java/src/teste/java/question4e5.java
@@ -0,0 +1,20 @@
+package teste.java;
+
+public class question4e5 {
+
+	public static void main(String[] args) {
+		System.out.println(" Calculo Hora Extra!! " );
+		int HorasTrablahadas = 180;
+		double calculo = 180 * 8 ;
+	    int adicional = 8 / 2;
+		double acrescimo = 180 * 4 + 1440; 
+	    System.out.println(" Quantidade de Horas trabalhadas por felippe = " + HorasTrablahadas + " hrs " );
+		System.out.println(" valor da hora trabalhada R$ 8.00 reais ");
+		System.out.println(" valor do salário de Felippe sem o adicional de 50%  = " + calculo  ) ;
+		System.out.println( " 50% do valor da hora trabalhada de felippe equevale  R$ " + adicional + " Reais  Sabendo disso temos a seguinte conclusão " );
+		System.out.println( " O salário de felippe esse més será = " + acrescimo );
+	
+	
+	}
+	
+}
